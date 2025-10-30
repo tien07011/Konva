619d88d9acfdd3fbb2a549fc0330bfc00584bdee
tien07011 <56587599+tien07011@users.noreply.github.com>
2025-10-30 04:07:31 +0000

    30 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import MilitarySymbolEditor from './components/MilitarySymbolEditor';
 
 function App() {
   return (
     <div className="App">
-      <MilitarySymbolEditor />
     </div>
   );
 }
